d2a28240f4e7f12e44febdd3e3ca8ce6a9e1be95
gageschmidt <gagemschmidt@gmail.com>
2020-06-27 23:43:46 -0500

    removed unnecessary index.js file, and refactored navigation js to actual class

--- frontend/src/navigation/navigation.js
+++ frontend/src/navigation/navigation.js
@@ -1,59 +1,54 @@
 import React from "react";
 import './navigation.css';
 
-function fontAwesomeCdn() {
-    let script = document.createElement('script');
-    script.src = "https://kit.fontawesome.com/f2647e425b.js";
-    script.crossOrigin = 'anonymous';
-    document.body.appendChild(script);
-}
 
-function moveRecipeOfTheDay() {
-    console.log('hello?');
-    let recipeElement = document.getElementById('recipe-otd');
-    let navigationElement = document.getElementsByClassName('nav');
-    navigationElement.onmouseover = function () {
-        recipeElement.setAttribute('style', 'left: 55%;')
+class Navigation extends React.Component {
+
+    fontAwesomeCdn() {
+        let script = document.createElement('script');
+        script.src = "https://kit.fontawesome.com/f2647e425b.js";
+        script.crossOrigin = 'anonymous';
+        document.body.appendChild(script);
     }
-}
 
-function Navigation() {
-    fontAwesomeCdn();
-    return (
-        <nav className="nav" onMouseOver={moveRecipeOfTheDay()}>
-            <ul className="nav-ul">
-                <li className="nav-item">
-                    <a className="nav-link" href="/">
-                        &nbsp;
-                        &nbsp;
-                        <i className="fas fa-hamburger fa-3x"></i>
-                        <span className="nav-text">
+   render() {
+        this.fontAwesomeCdn();
+        return (
+            <nav className="nav">
+                <ul className="nav-ul">
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">
+                            &nbsp;
+                            &nbsp;
+                            <i className="fas fa-hamburger fa-3x"></i>
+                            <span className="nav-text">
                            &nbsp; Homepage
                         </span>
-                    </a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="/submit">
-                        &nbsp;
-                        &nbsp;
-                        <i className="fas fa-pizza-slice  fa-3x"></i>
-                        <span className="nav-text">
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/submit">
+                            &nbsp;
+                            &nbsp;
+                            <i className="fas fa-pizza-slice  fa-3x"></i>
+                            <span className="nav-text">
                          &nbsp; Submit Recipe
                         </span>
-                    </a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">
-                        &nbsp;
-                        &nbsp;
-                        <i className="fas fa-carrot  fa-3x"></i>
-                        <span className="nav-text">
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">
+                            &nbsp;
+                            &nbsp;
+                            <i className="fas fa-carrot  fa-3x"></i>
+                            <span className="nav-text">
                          &nbsp; Nothing yet
                         </span>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    )}
-
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        )
+    }
+}
 export default Navigation;
